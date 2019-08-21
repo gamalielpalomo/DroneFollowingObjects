4a3c52969b49d49c3fe658dd4d6e8278518089d4
Tania <rodriguez.200936280@gmail.com>
2019-08-21 14:48:50 -0500

    First commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tania/catkin_ws/src/drone_application
+# Build directory: /home/tania/catkin_ws/src/drone_application
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
